f098c0f0a63724d45647e1b9e209067d17ed7395
Dustin Olenslager <94700316+dustin-olenslager@users.noreply.github.com>
2026-03-07 15:10:06 -0700

    fix(android): remove FLAG_SECURE to allow user screenshots

--- apps/android/android/app/src/main/java/dev/plexo/Plexo/MainActivity.java
+++ apps/android/android/app/src/main/java/dev/plexo/Plexo/MainActivity.java
@@ -1,18 +1,11 @@
 package dev.plexo.Plexo;
 
 import android.os.Bundle;
-import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // Android FLAG_SECURE
-        getWindow().setFlags(
-            WindowManager.LayoutParams.FLAG_SECURE,
-            WindowManager.LayoutParams.FLAG_SECURE
-        );
     }
 }
